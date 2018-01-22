f4dda48ee1430f4adfb3c4f87e3de60ae72c31f5
Tran Văn Tien <tvtien139@gmail.com>
2018-01-23 00:08:39 +0700

    update

--- common/models/articles.js
+++ common/models/articles.js
@@ -2,6 +2,8 @@
 
 const app = require('../../server/server.js');
 const cst = require('./constant.js');
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 module.exports = function(Articles) {
 
@@ -78,8 +80,16 @@ module.exports = function(Articles) {
     }).then(results=>{
       if(!results){
         results = {};
+        cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, results);
+      } else {
+        let total_view = results.total_view ? results.total_view + 1 : 1;
+
+        Articles.updateAll({"id":results.id}, {total_view: total_view}).then(data=>{
+          cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, results);
+        });
+
       }
-      cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS,results);
+
     }).catch(err=>{
       cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
     });
@@ -204,6 +214,156 @@ module.exports = function(Articles) {
       accepts: [
         {arg: 'user_id', type:'string',required: true},
         {arg: 'article_id', type:'string', required: true},
+      ],
+      returns: [
+        {arg: 'code', type:'number'},
+        {arg: 'message', type:'string'},
+        {arg: 'data', type:'object'}
+      ]
+    }
+  );
+
+  Articles.likeArticle = function (user_id, article_id, type, cb) {
+
+    user_id = user_id.toString();
+    article_id = article_id.toString();
+
+    Promise.all([ Articles.findOne({where: {id: article_id}}), app.models.Users.findOne({where: {id: user_id}})]).spread((article,user)=>{
+
+        if(article && user){
+          let obj_like = {user_id:user_id, type:type};
+
+          let likes = article.likes.filter(obj=>{
+              return obj.user_id !== user_id
+          });
+          if(type !== 0){
+            likes.unshift(obj_like);
+          }
+          let total_like = likes.length;
+          let like_icon = _.uniqBy(likes,'type');
+          like_icon = _.map(like_icon,'type');
+
+          Articles.updateAll({id: article_id},{likes:likes, total_like:total_like, like_icon:like_icon}).then(val=>{
+            cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, {likes:likes, total_like:total_like, like_icon:like_icon});
+          });
+
+        } else {
+          cb(null, cst.HTTP_CODE_FAILED_DATA, 'Không tồn tại user hoặc bài viết', {});
+        }
+
+    }).catch(err=>{
+      cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
+    });
+  }
+
+  Articles.remoteMethod(
+    'likeArticle',
+    {
+      http: {verb:'post'},
+      accepts: [
+        {arg: 'user_id', type:'string',required: true},
+        {arg: 'article_id', type:'string', required: true},
+        {arg: 'type', type:'number', required: true, description:"nhap cac icon tu 1 den 6"},
+      ],
+      returns: [
+        {arg: 'code', type:'number'},
+        {arg: 'message', type:'string'},
+        {arg: 'data', type:'object'}
+      ]
+    }
+  );
+
+  Articles.getHotArticle = function (user_id, type,size, page, cb) {
+
+    size = parseInt(size, 10);
+    page = parseInt(page, 10);
+    let skip = (page - 1)*size;
+
+    let where = {status: "Publish"};
+
+    let order = "total_view DESC";
+    if(type === 3){
+      order = "total_like DESC";
+    }
+
+    if(type === 4){
+      order = "total_comment DESC";
+    }
+
+    if(type === 5){
+      order = "total_share DESC";
+    }
+
+    Articles.find({
+      include:{
+        relation: "getUser",
+        scope:{
+          fields: ["image","userSlug","name"]
+        }
+      },
+      where: where,
+      skip: skip,
+      limit: size,
+      order: order
+    }).then(results=>{
+
+      if(!results){
+        results = [];
+      }
+
+      let next_page = results.length - size < 0 ? -1 : page + 1;
+
+      cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, {results, next_page});
+    }).catch(err=>{
+      cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
+    });
+
+  }
+
+  Articles.remoteMethod(
+    'getHotArticle',
+    {
+      http: {verb:'post'},
+      accepts: [
+        {arg: 'user_id', type:'string',required: true},
+        {arg: 'type', type:'number', required: true, description:"nhap cac icon tu 1 den 5, 1 la all, 2 la theo view, 3 là theo like, 4 là theo coment,5 la theo share"},
+        {arg: 'size', type:'number', default: 10},
+        {arg: 'page', type:'number', default: 1}
+      ],
+      returns: [
+        {arg: 'code', type:'number'},
+        {arg: 'message', type:'string'},
+        {arg: 'data', type:'object'}
+      ]
+    }
+  );
+
+  Articles.getRandomArticle = function (user_id, cb) {
+
+    Articles.find({
+      sample: {
+        size: 10
+      }
+    }).then(results=>{
+
+      if(!results){
+        results = [];
+      }
+
+      cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, {results, next_page:-1});
+
+    }).catch(err=>{
+      cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
+    });
+
+  }
+
+  Articles.remoteMethod(
+    'getRandomArticle',
+    {
+      http: {verb:'post'},
+      accepts: [
+        {arg: 'user_id', type:'string',required: true}
       ],
       returns: [
         {arg: 'code', type:'number'},
--- common/models/comment.js
+++ common/models/comment.js
@@ -53,7 +53,10 @@ module.exports = function (Comments) {
       if(user && comment && content){
         let data = {created_by: user.id, content:content,comment_id:comment.id, type: 2, created_at: Date.now(), like:0}
         Comments.create(data).then(val=>{
-          cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, val);
+          let count_reply = comment.count_reply + 1;
+          Comments.updateAll({where: {_id: comment_id}},{count_reply: count_reply}).then(result=>{
+            cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS, result);
+          });
         });
       } else {
         cb(null, cst.HTTP_CODE_MISSING_PARAM, cst.MESSAGE_GET_MISSING_PARAM, {});
@@ -136,7 +139,9 @@ module.exports = function (Comments) {
       if(!results){
         results = [];
       }
+
       let next_page = results.length - size < 0 ? -1 : page + 1;
+
       cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS,{results, next_page});
     }).catch(err=>{
       cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
@@ -150,6 +155,58 @@ module.exports = function (Comments) {
       accepts: [
         {arg: 'user_id', type:'string',required: true},
         {arg: 'article_id', type:'string', required: true},
+        {arg: 'size', type:'number', default: 10},
+        {arg: 'page', type:'number', default: 1}
+      ],
+      returns: [
+        {arg: 'code', type:'number'},
+        {arg: 'message', type:'string'},
+        {arg: 'data', type:'object'}
+      ]
+    }
+  );
+
+  Comments.getReplyComment = function (user_id, comment_id ,size, page, cb) {
+
+    size = parseInt(size, 10);
+    page = parseInt(page, 10);
+    let skip = (page - 1)*size;
+
+    Comments.find({
+      include:{
+        relation: "getUser",
+        scope:{
+          fields: ["image","userSlug","name"]
+        }
+      },
+      where: {
+        type: 2,
+        comment_id: comment_id
+      },
+      skip: skip,
+      limit: size,
+      order: "created_at DESC"
+    }).then(results=>{
+
+      if(!results){
+        results = [];
+      }
+
+      let next_page = results.length - size < 0 ? -1 : page + 1;
+
+      cb(null, cst.HTTP_CODE_SUCCESS, cst.MESSAGE_GET_SUCCESS,{results, next_page});
+    }).catch(err=>{
+      cb(null,cst.HTTP_CODE_FAILED_DATA, cst.MESSAGE_GET_FAILED, err);
+    });
+  }
+
+  Comments.remoteMethod(
+    'getReplyComment',
+    {
+      http: {verb:'post'},
+      accepts: [
+        {arg: 'user_id', type:'string',required: true},
+        {arg: 'comment_id', type:'string', required: true},
         {arg: 'size', type:'number', default: 10},
         {arg: 'page', type:'number', default: 1}
       ],
